2c531138adf89628c8d71927bc557e96e9bdbcf9
robstephenson <robstephenson@digital.landregistry.gov.uk>
2015-06-23 10:55:03 +0100

    code review changes

--- application/routes.py
+++ application/routes.py
@@ -1,23 +1,26 @@
 from application import app
-from flask import Response, request, url_for
+from flask import Response, request
 from datetime import datetime
+
 
 @app.route('/', methods=["GET"])
 def index():
-	return Response(status=200)
+    return Response(status=200)
+
 
 @app.route('/register', methods=["POST"])
 def register():
-	if request.headers['Content-Type'] != "application/json":
-		return Response(status=415)  # 415 (Unsupported Media Type)
+    if request.headers['Content-Type'] != "application/json":
+        return Response(status=415)  # 415 (Unsupported Media Type)
 
-	time = datetime.now()
+    time = datetime.now()
 
-	json_data = request.get_json(force=True)
-	record = open("banksReg.txt","a")
-	record.write('Registration received %s/%s/%s at %s:%s:%s'%(time.day,time.month,time.year,time.hour,time.minute,time.second))
-	record.write('\n')
-	record.write(str(json_data) + '\n' + '\n')
-	record.close()
+    json_data = request.get_json(force=True)
+    record = open("banksReg.txt", "a")
+    record.write('Registration received %s/%s/%s at %s:%s:%s' % (
+        time.day, time.month, time.year, time.hour, time.minute, time.second))
+    record.write('\n')
+    record.write('{}\n\n'.format(str(json_data)))
+    record.close()
 
-	return Response(status=200)
+    return Response(status=200)
