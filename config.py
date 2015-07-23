9a09b6fdcd26fbacfa73574835da1fe27a8760f6
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-07-23 14:44:25 +0100

    Add separate config for preview.

--- config.py
+++ config.py
@@ -4,8 +4,14 @@ class Config(object):
     DEBUG = False
 
 
-class DevelopmentConfig(object):
+class DevelopmentConfig(Config):
     DEBUG = True
+    RULES_ENGINE_URL = "http://localhost:5005"
+    BANKRUPTCY_DATABASE_API = "http://localhost:5004"
+    CASEWORK_DATABASE_API = "http://localhost:5006"
+
+
+class PreviewConfig(Config):
     RULES_ENGINE_URL = "http://localhost:5005"
     BANKRUPTCY_DATABASE_API = "http://localhost:5004"
     CASEWORK_DATABASE_API = "http://localhost:5006"
