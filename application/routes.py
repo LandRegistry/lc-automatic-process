cf276561e8f72814515b016dc71a0aae4a34a32e
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-10-16 13:51:32 +0100

    Healthcheck route & SPK8-03

--- application/routes.py
+++ application/routes.py
@@ -5,9 +5,47 @@ import requests
 import json
 
 
+def check_bankreg_health():
+    return requests.get(app.config['BANKRUPTCY_DATABASE_API'] + '/health')
+
+
+def check_automat_health():
+    return requests.get(app.config['RULES_ENGINE_URL'] + '/health')
+
+
+application_dependencies = [
+    {
+        "name": "bankruptcy-registration",
+        "check": check_bankreg_health
+    },
+    {
+        "name": "automation_rules",
+        "check": check_automat_health
+    }
+]
+
+
 @app.route('/', methods=["GET"])
 def index():
     return Response(status=200)
+
+
+@app.route('/health', methods=['GET'])
+def health():
+    result = {
+        'status': 'OK',
+        'dependencies': {}
+    }
+
+    status = 200
+    for dependency in application_dependencies:
+        response = dependency["check"]()
+        result['dependencies'][dependency['name']] = str(response.status_code) + ' ' + response.reason
+        data = json.loads(response.content.decode('utf-8'))
+        for key in data['dependencies']:
+            result['dependencies'][key] = data['dependencies'][key]
+
+    return Response(json.dumps(result), status=status, mimetype='application/json')
 
 
 @app.route('/register', methods=["POST"])
@@ -26,14 +64,22 @@ def register():
         go_auto = (data['register_auto'])
         if go_auto:
             logging.info('Automatically processing')
-            url = app.config['BANKRUPTCY_DATABASE_API'] + '/register'
+
+            # Convert the data for banks-reg, though in time BR will also change
+            json_data['date'] = json_data['application_date']
+            json_data['debtor_name'] = json_data['debtor_names'][0]
+            json_data['debtor_alternative_name'] = json_data['debtor_names'][1:]
+            del json_data['application_date']
+            del json_data['debtor_names']
+
+            url = app.config['BANKRUPTCY_DATABASE_API'] + '/registration'
             headers = {'Content-Type': 'application/json'}
             response = requests.post(url, data=json.dumps(json_data), headers=headers)
 
         else:
             # save to work list
             logging.info('Dropping to manual')
-            url = app.config['CASEWORK_DATABASE_API'] + '/lodge_manual'
+            url = app.config['CASEWORK_DATABASE_API'] + '/workitem'
             headers = {'Content-Type': 'application/json'}
             response = requests.post(url, data=json.dumps(json_data), headers=headers)
             if response.status_code == 200:
@@ -41,7 +87,7 @@ def register():
                 work_id = (data['id'])
                 return Response(json.dumps({'id': work_id}), status=response.status_code)
 
-        return Response(status=response.status_code)
+        return Response(response.content, status=response.status_code, mimetype='application/json')
     else:
         logging.error('Received code %d', response.status_code)
         return Response(status=response.status_code)
