0f22dfad1da4aa3cca1b381a797cde018de1b584
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-11-17 15:36:37 +0000

    Remove need to automation_rules application

--- application/routes.py
+++ application/routes.py
@@ -8,21 +8,16 @@ import json
 def check_bankreg_health():
     return requests.get(app.config['BANKRUPTCY_DATABASE_API'] + '/health')
 
-
-def check_automat_health():
-    return requests.get(app.config['RULES_ENGINE_URL'] + '/health')
-
-
 application_dependencies = [
     {
         "name": "bankruptcy-registration",
         "check": check_bankreg_health
-    },
-    {
-        "name": "automation_rules",
-        "check": check_automat_health
     }
 ]
+
+
+def automatic_process(registration):
+    return True
 
 
 @app.route('/', methods=["GET"])
@@ -48,46 +43,37 @@ def health():
     return Response(json.dumps(result), status=status, mimetype='application/json')
 
 
-@app.route('/register', methods=["POST"])
+@app.route('/bankruptcies', methods=["POST"])
 def register():
     if request.headers['Content-Type'] != "application/json":
         return Response(status=415)  # 415 (Unsupported Media Type)
 
+    respond_with = ""
+    respond_code = 200
     json_data = request.get_json(force=True)
-
-    url = app.config['RULES_ENGINE_URL'] + '/check_auto'
-    headers = {'Content-Type': 'application/json'}
-    response = requests.post(url, data=json.dumps(json_data), headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        go_auto = (data['register_auto'])
-        if go_auto:
-            logging.info('Automatically processing')
-
-            # Convert the data for banks-reg, though in time BR will also change
-            json_data['date'] = json_data['application_date']
-            json_data['debtor_name'] = json_data['debtor_names'][0]
-            json_data['debtor_alternative_name'] = json_data['debtor_names'][1:]
-            del json_data['application_date']
-            del json_data['debtor_names']
-
-            url = app.config['BANKRUPTCY_DATABASE_API'] + '/registration'
-            headers = {'Content-Type': 'application/json'}
-            response = requests.post(url, data=json.dumps(json_data), headers=headers)
-
-        else:
-            # save to work list
-            logging.info('Dropping to manual')
-            url = app.config['CASEWORK_DATABASE_API'] + '/workitem'
-            headers = {'Content-Type': 'application/json'}
-            response = requests.post(url, data=json.dumps(json_data), headers=headers)
-            if response.status_code == 200:
-                data = response.json()
-                work_id = (data['id'])
-                return Response(json.dumps({'id': work_id}), status=response.status_code)
-
-        return Response(response.content, status=response.status_code, mimetype='application/json')
+    if automatic_process(json_data):
+        logging.info('Automatically processing')
+        # Convert the data for banks-reg, though in time BR will also change
+        json_data['date'] = json_data['application_date']
+        json_data['debtor_name'] = json_data['debtor_names'][0]
+        json_data['debtor_alternative_name'] = json_data['debtor_names'][1:]
+        del json_data['application_date']
+        del json_data['debtor_names']
+        url = app.config['BANKRUPTCY_DATABASE_API'] + '/registration'
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(url, data=json.dumps(json_data), headers=headers)
+        respond_with = response.content
+        respond_code = response.status_code
     else:
-        logging.error('Received code %d', response.status_code)
-        return Response(status=response.status_code)
+        # save to work list
+        logging.info('Dropping to manual')
+        url = app.config['CASEWORK_DATABASE_API'] + '/workitem'
+        headers = {'Content-Type': 'application/json'}
+        response = requests.post(url, data=json.dumps(json_data), headers=headers)
+        if response.status_code == 200:
+            data = response.json()
+            work_id = (data['id'])
+            respond_with = json.dumps({'id': work_id})
+        respond_code = response.status_code
+
+    return Response(respond_with, status=respond_code, mimetype='application/json')
--- config.py
+++ config.py
@@ -6,12 +6,10 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    RULES_ENGINE_URL = "http://localhost:5005"
     BANKRUPTCY_DATABASE_API = "http://localhost:5004"
     CASEWORK_DATABASE_API = "http://localhost:5006"
 
 
 class PreviewConfig(Config):
-    RULES_ENGINE_URL = "http://localhost:5005"
     BANKRUPTCY_DATABASE_API = "http://localhost:5004"
     CASEWORK_DATABASE_API = "http://localhost:5006"
