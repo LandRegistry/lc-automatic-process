c9b2e65b187c53712231399079cd18943e3d6804
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2016-01-18 07:27:41 +0000

    belated commit of amalgamate names

--- application/routes.py
+++ application/routes.py
@@ -55,10 +55,10 @@ def register():
         logging.info('Automatically processing')
         # Convert the data for banks-reg, though in time BR will also change
         json_data['date'] = json_data['application_date']
-        json_data['debtor_name'] = json_data['debtor_names'][0]
-        json_data['debtor_alternative_name'] = json_data['debtor_names'][1:]
+        # json_data['debtor_name'] = json_data['debtor_names'][0]
+        # json_data['debtor_alternative_name'] = json_data['debtor_names'][1:]
         del json_data['application_date']
-        del json_data['debtor_names']
+        # del json_data['debtor_names']
         url = app.config['BANKRUPTCY_DATABASE_API'] + '/registrations'
         headers = {'Content-Type': 'application/json'}
         response = requests.post(url, data=json.dumps(json_data), headers=headers)
