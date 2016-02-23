9d6b5e80c124838ff12b4d45c3108b4439e2d37c
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2016-02-23 10:13:37 +0000

    Get config from environment

--- application/__init__.py
+++ application/__init__.py
@@ -4,6 +4,6 @@ from log.logger import setup_logging
 
 
 app = Flask(__name__)
-app.config.from_object(os.environ.get('SETTINGS'))
+app.config.from_object('config.Config')
 
 setup_logging(app.config)
--- config.py
+++ config.py
@@ -1,24 +1,13 @@
+import os
 
 
 class Config(object):
     DEBUG = False
     APPLICATION_NAME = 'lc-automatic-process'
+    BANKRUPTCY_DATABASE_API = os.getenv('LAND_CHARGES_URL', 'http://localhost:5004')
+    CASEWORK_DATABASE_API = os.getenv('CASEWORK_API_URL', "http://localhost:5006")
+    MQ_USERNAME = os.getenv("MQ_USERNAME", "mquser")
+    MQ_PASSWORD = os.getenv("MQ_PASSWORD", "mqpassword")
+    MQ_HOSTNAME = os.getenv("MQ_HOST", "localhost")
+    MQ_PORT = os.getenv("MQ_PORT", "5672")
 
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    BANKRUPTCY_DATABASE_API = "http://localhost:5004"
-    CASEWORK_DATABASE_API = "http://localhost:5006"
-    MQ_USERNAME = "mquser"
-    MQ_PASSWORD = "mqpassword"
-    MQ_HOSTNAME = "localhost"
-    MQ_PORT = "5672"
-
-
-class PreviewConfig(Config):
-    BANKRUPTCY_DATABASE_API = "http://localhost:5004"
-    CASEWORK_DATABASE_API = "http://localhost:5006"
-    MQ_USERNAME = "mquser"
-    MQ_PASSWORD = "mqpassword"
-    MQ_HOSTNAME = "localhost"
-    MQ_PORT = "5672"
